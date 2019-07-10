b3edcbfdfb73cd49dfa1065d982bb3d0e8d99368
Quinten Justus <qschorsij@outlook.com>
2019-07-10 05:32:21 +0200

    [FEATURE] Added commands [FIX] Fixed travis script

---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Protoqol\Prequel\Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * Class InstallCommand
+ *
+ * @package App\Console\Commands
+ */
+class InstallCommand extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'prequel:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install Prequel, publishing its ServiceProvider, config and assets.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment('Publishing Prequel Service Provider...');
+        $this->callSilent('vendor:publish', [
+            '--provider' => 'Protoqol\Prequel\PrequelServiceProvider',
+        ]);
+
+        $this->comment('Publishing Prequel Assets...');
+        $this->callSilent('vendor:publish', [
+            '--tag' => 'prequel-assets',
+        ]);
+
+        $this->comment('Publishing Prequel Config...');
+        $this->callSilent('vendor:publish', [
+            '--tag' => 'prequel-config',
+        ]);
+
+        $this->info('Prequel succesfully installed.');
+    }
+}
