ebc9cbf3f3e9588603a1b6d5fa41473cebb13b6b
Aaron Saray <me@aaronsaray.com>
2019-07-15 06:48:49 -0500

    giving routes a prefix and name - so in unit tests we can refer to them by name instead of by path.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,12 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Protoqol\Prequel\Http\Controllers')
     ->middleware('Protoqol\Prequel\Http\Middleware\Authorised')
     ->prefix(config('prequel.path'))
+    ->name('prequel.')
     ->group(function () {
 
         /**
          * Main view route
          */
-        Route::get('/', 'PrequelController@index');
+        Route::get('/', 'PrequelController@index')->name('index');
 
 
         /**
